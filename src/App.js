66d2f98597b58411f50a3fc34808b9b20f9ae3bb
KevalPolara <kevalpolara5272@gmail.com>
2023-09-05 16:38:26 +0530

    components

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Folder Structuer
+          Learn React Components
         </a>
       </header>
     </div>
